68b37d7c625bcad60837a30f8572284de438470f
juaco <juaco@LAPTOP-23S2M07S>
2022-04-17 23:53:17 -0300

    tp

--- TP2_Grupo40/src/main/java/ar/edu/unju/fi/aplicacion/p8/Punto8.java
+++ TP2_Grupo40/src/main/java/ar/edu/unju/fi/aplicacion/p8/Punto8.java
@@ -3,7 +3,12 @@ package ar.edu.unju.fi.aplicacion.p8;
 public class Punto8 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	    int i = 0;
+	    
+	    do {
+	      System.out.println(i);
+	      i+=4;
+	    } while(i <= 100);
 
 	}
 
