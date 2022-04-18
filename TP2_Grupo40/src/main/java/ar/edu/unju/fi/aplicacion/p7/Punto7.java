65795a90a75dc3c0064a1b6443bae393aab9fa2c
juaco <juaco@LAPTOP-23S2M07S>
2022-04-17 23:55:21 -0300

    tp

--- TP2_Grupo40/src/main/java/ar/edu/unju/fi/aplicacion/p7/Punto7.java
+++ TP2_Grupo40/src/main/java/ar/edu/unju/fi/aplicacion/p7/Punto7.java
@@ -1,10 +1,27 @@
 package ar.edu.unju.fi.aplicacion.p7;
 
+import java.util.Scanner;
 public class Punto7 {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	Scanner sc = new Scanner(System.in);
+       int j=40;
+       int k=0;
+        for (int i=2; i<=j; i++) {
+        	i=i++;
+        	k=j+i-1;	
+        	
+        if (i % 2 != 0)  {
+        		//System.out.println("impar"+ i);
+        		//System.out.println(k);
+        		
+        	 k=k+i;
+        		System.out.println(k);
+        	} 
+        	else {
+        		//System.out.println("IMPAR"+i);
+        	}
+        }
 	}
-
 }
